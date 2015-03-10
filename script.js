3523943033dc75ccdfa8f09995b4ec8d753cd26d
Patrick-Laptop <napscraft@gmail.com>
2015-03-10 08:57:42 +0100

    Added auto title change

--- script.js
+++ script.js
@@ -10,21 +10,21 @@ var bgImage = new Image();
 bgImage.onload = function () {
 	bgReady = true;
 };
-bgImage.src = "http://i.imgur.com/XWctBLP.png";
+bgImage.src = "http://i.imgur.com/XWctBLP.png"; //external source.
 
 var heroReady = false;
 var heroImage = new Image();
 heroImage.onload = function () {
 	heroReady = true;
 };
-heroImage.src = "http://i.imgur.com/3ppJxts.png";
+heroImage.src = "http://i.imgur.com/3ppJxts.png"; //external source.
 
 var monsterReady = false;
 var monsterImage = new Image();
 monsterImage.onload = function () {
 	monsterReady = true;
 };
-monsterImage.src = "http://i.imgur.com/47yUjMS.png";
+monsterImage.src = "http://i.imgur.com/47yUjMS.png"; //external source
 
 var hero = {
 	speed: 256 
@@ -67,8 +67,11 @@ var update = function (modifier) {
 	if (
 		hero.x <= (monster.x + 32)&& monster.x <= (hero.x + 32)&& hero.y <= (monster.y + 32)&& monster.y <= (hero.y + 32)) {
 		++monstersCaught;
+	if (monstersCaught > 0) {
+		document.title = monstersCaught + " monsters caught!";
 		reset();
 	}
+}
 	
 	if (hero.x < 0) hero.x = 0;
 	if (hero.y < 0) hero.y = 0;
@@ -90,10 +93,10 @@ var render = function () {
 	}
 
 	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
+	ctx.font = "19px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+	ctx.fillText("Monsters caught: " + monstersCaught, 32, 32);
 };
 
 var main = function () {
